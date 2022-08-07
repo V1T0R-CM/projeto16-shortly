b44a4ea031090390de78c6141714916c6e5cc7a1
Vitor Carvalho Matos <vitorcarvalhomatos@gmail.com>
2022-08-07 10:03:24 -0300

    realizacao dump

--- dump.sql
+++ dump.sql
@@ -1,17 +1,182 @@
-CREATE DATABASE "shortly-database";
+--
+-- PostgreSQL database dump
+--
 
-CREATE TABLE users (
-	id SERIAL NOT NULL PRIMARY KEY,
-	name TEXT NOT NULL,
-	email TEXT NOT NULL UNIQUE,
-	password TEXT NOT NULL
+-- Dumped from database version 12.11 (Ubuntu 12.11-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.11 (Ubuntu 12.11-0ubuntu0.20.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: url; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.url (
+    id integer NOT NULL,
+    "userId" integer,
+    "shortUrl" text NOT NULL,
+    "fullUrl" text NOT NULL,
+    "visitCount" integer DEFAULT 0 NOT NULL,
+    "createdAt" date DEFAULT now() NOT NULL
 );
 
 
-CREATE TABLE url (
-	id SERIAL NOT NULL PRIMARY KEY,
-    "userId" INTEGER REFERENCES users(id),
-	"shortUrl" TEXT NOT NULL,
-    "fullUrl" TEXT NOT NULL,
-	"visitCount" INTEGER NOT NULL DEFAULT 0
+ALTER TABLE public.url OWNER TO postgres;
+
+--
+-- Name: url_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.url_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.url_id_seq OWNER TO postgres;
+
+--
+-- Name: url_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.url_id_seq OWNED BY public.url.id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name text NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL,
+    "createdAt" date DEFAULT now() NOT NULL
 );
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: url id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.url ALTER COLUMN id SET DEFAULT nextval('public.url_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: url; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.url (id, "userId", "shortUrl", "fullUrl", "visitCount", "createdAt") FROM stdin;
+2	2	ZXUWksMdH-	https://github.com/V1T0R-CM/projeto9-cineflex/blob/main/src/index.js	0	2022-08-06
+4	2	B4g2jFmSat	https://github.com/V1T0R-CM/Projeto_05_batePapoUOL/blob/main/index.html	1	2022-08-06
+3	2	x16bAtd6-K	https://github.com/V1T0R-CM/projeto_07_instagramreact/blob/main/src/index.js	1	2022-08-06
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, name, email, password, "createdAt") FROM stdin;
+2	Joãozin	joaozin@driven.com.br	$2b$10$4qlTopc6AjH3h31ckxAo.uZCO084WuKOhd4ydjsi5.XCSR3An5ade	2022-08-06
+3	João	joao@driven.com.br	$2b$10$i9NMd.P2xasPCtKwVZj75ei3g0u.Lbz4IWz4kiiBBXCu8i3ht7A4G	2022-08-06
+\.
+
+
+--
+-- Name: url_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.url_id_seq', 4, true);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 3, true);
+
+
+--
+-- Name: url url_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.url
+    ADD CONSTRAINT url_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: url url_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.url
+    ADD CONSTRAINT "url_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- src/db/database.js
+++ src/db/database.js
@@ -4,11 +4,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = pkg;
+
+//const connection = new Pool({
+//  host: process.env.DATABASE_SERVER,
+//  port: process.env.DATABASE_PORT,
+//  user: process.env.DATABASE_USER,
+//  password: process.env.DATABASE_PASSWORD,
+//  database: process.env.DATABASE_NAME
+//});
+
 const connection = new Pool({
-   connectionString: process.env.DATABASE_URL,
-   ssl:{
-       rejectUnauthorized: false
-   }
-});
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }
+  });
 
 export default connection;
